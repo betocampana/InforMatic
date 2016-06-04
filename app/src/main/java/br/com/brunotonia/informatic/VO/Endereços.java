06c850b028a1184717f725c4cfc2a6b0e668dade
brunotonia <bruno.rvt@gmail.com>
2016-06-03 21:46:49 -0300

    Initial commit

---
+++ app/src/main/java/br/com/brunotonia/informatic/VO/Endereços.java
@@ -0,0 +1,14 @@
+package br.com.brunotonia.informatic.VO;
+
+/**
+ * Created by bruno on 03/06/16.
+ */
+public class Endereços {
+
+    private Long id;
+    private Long cliente;
+    private String rua;
+    private String numero;
+    private String cidade;
+    
+}
