d74a1495650a6b5bb01d639d385805e876ed220c
Beto Campana <betocoke@yahoo.com.br>
2016-06-10 20:20:14 -0300

    isso ai

--- app/src/main/java/br/com/brunotonia/informatic/VIEW/ServicoMenuActivity.java
+++ app/src/main/java/br/com/brunotonia/informatic/VIEW/ServicoMenuActivity.java
@@ -3,11 +3,11 @@ package br.com.brunotonia.informatic.VIEW;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TesteGit extends AppCompatActivity {
+public class ServicoMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teste_git);
+        setContentView(R.layout.activity_servico_menu);
     }
 }
